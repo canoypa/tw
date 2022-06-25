1c8867b3a43cd5aed644a64c5ff9d47f9c20e1c1
Cano <contact.sasasaky@gmail.com>
2022-06-25 13:15:09 +0900

    root command

---
+++ cmd/root.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func RootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "tw",
+		Run: func(cmd *cobra.Command, args []string) {
+			text, err := cmd.Flags().GetString("text")
+
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			if text == "" {
+				cmd.Help()
+				return
+			}
+
+			fmt.Println("ToDo: Tweet", text)
+		},
+	}
+
+	cmd.Flags().StringP("text", "t", "", "Tweet text")
+
+	return cmd
+}
