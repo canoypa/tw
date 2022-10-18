6e8ff9f53d9484378dfe67cc82a81eab4ac0fa6a
Cano <contact.sasasaky@gmail.com>
2022-10-18 11:52:37 +0900

    init config file

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func RootCmd() *cobra.Command {
@@ -28,4 +33,27 @@ func RootCmd() *cobra.Command {
 	cmd.Flags().StringP("text", "t", "", "Tweet text")
 
 	return cmd
+}
+
+func init() {
+	cobra.OnInitialize(initializeConfig)
+}
+
+func initializeConfig() {
+	homePath, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+
+	configPath := filepath.Join(homePath, ".twcli")
+	configName := "hosts"
+	configType := "yaml"
+
+	viper.AddConfigPath(configPath)
+	viper.SetConfigName(configName)
+	viper.SetConfigType(configType)
+
+	// if config not found
+	if err := viper.ReadInConfig(); err != nil {
+		os.MkdirAll(configPath, 0700)
+		viper.WriteConfigAs(filepath.Join(configPath, fmt.Sprintf("%s.%s", configName, configType)))
+	}
 }
